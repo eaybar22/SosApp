9e5982be5e8582d1de3c6e89ffd266810b9df116
eaybar22 <44527459+eaybar22@users.noreply.github.com>
2018-12-06 10:55:55 -0400

    Este commit no me deja virtualizar la app I# On branch master    (use "git push" to publish your local commits)

--- app/src/main/java/ado/edu/itla/sosapp/repositorio/DbConexion.java
+++ app/src/main/java/ado/edu/itla/sosapp/repositorio/DbConexion.java
@@ -1,11 +1,33 @@
-package ado.edu.itla.sosapp;
+package ado.edu.itla.sosapp.repositorio;
 
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
 public class DbConexion extends SQLiteOpenHelper {
+    public DbConexion(Context context, String name, SQLiteDatabase.CursorFactory factory, int vesion) {
+        super(context, name, factory, vesion);
+    }
 
+    public static String getTablaUsuario() {
+        return TABLA_USUARIO;
+    }
+
+    public static void setTablaUsuario(String tablaUsuario) {
+        TABLA_USUARIO = tablaUsuario;
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL(TABLA_USUARIO);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("DROP TABLE IF EXISTS usuario");
+        onCreate(db);
+
+    }
     private static String NOMBRE_BASEDATOS = "sosapp_db.db";
     private static int VERSION_BASEDATOS = 1;
     private static String TABLA_USUARIO = "CREATE TABLE usuario (" +
@@ -16,22 +38,12 @@ public class DbConexion extends SQLiteOpenHelper {
             ")";
 
 
-    public DbConexion(Context context, String name, SQLiteDatabase.CursorFactory factory, int vesion) {
-        super(context, name, factory, vesion);
-    }
+
 
 //Creando la base de datos.
     public DbConexion(Context context) {
         super(context, NOMBRE_BASEDATOS, null, VERSION_BASEDATOS);
     }
 
-    @Override
-    public void onCreate(SQLiteDatabase db) {
-        db.execSQL(TABLA_USUARIO);
-    }
 
-    @Override
-    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-
-    }
 }
---
+++ app/src/main/java/ado/edu/itla/sosapp/repositorio/usuario/UsuarioRepositorioImpl.java
@@ -0,0 +1,60 @@
+package ado.edu.itla.sosapp.repositorio.usuario;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+
+import ado.edu.itla.sosapp.entidad.Usuario;
+import ado.edu.itla.sosapp.repositorio.DbConexion;
+
+
+public class UsuarioRepositorioImpl implements UsuarioRepositorio {
+    private DbConexion dbConexion;
+
+    public UsuarioRepositorioImpl(Context context){
+        this.dbConexion = new DbConexion(context);
+    }
+
+    @Override
+    public void guardar(Usuario usuario) {
+
+        ContentValues cv = new ContentValues();
+        cv.put("email", usuario.getEmail());
+        cv.put("password", usuario.getPassword());
+        cv.put("nombre", usuario.getNombre());
+
+        SQLiteDatabase db = dbConexion.getReadableDatabase();
+
+        Long id = db.insert("usuario", null, cv);
+        usuario.setId(id.intValue());
+    }
+
+    @Override
+    public Usuario buscar(String email) {
+
+        //Columnas a buscar en la base de datos.
+        String columnas[] = new String[]{"id", "password", "nombre"};
+
+        //abrir conexion a la base de datos (SOLO LECTURA).
+        SQLiteDatabase db = dbConexion.getReadableDatabase();
+
+        //Ejecutamos la consulta.
+        Cursor cursor = db.query("usuario", columnas, "email=?", new String[]{email}, null, null, null);
+
+        Usuario usuario = null;
+        if (cursor.moveToFirst()){
+
+            usuario = new Usuario();
+
+            usuario.setId(cursor.getInt(cursor.getColumnIndex("id")));
+            usuario.setNombre(cursor.getString(cursor.getColumnIndex("nombre")));
+            usuario.setEmail(cursor.getString(cursor.getColumnIndex("email")));
+            usuario.setPassword(cursor.getString(cursor.getColumnIndex("password")));
+
+            cursor.close();
+            db.close();
+        }
+        return usuario;
+    }
+}
